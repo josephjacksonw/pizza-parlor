d91f9570b4b923c9ff1be7ffe60a51fcb88e7608
josephjacksonw <josephjacksonw@gmail.com>
2022-08-26 14:50:30 -0400

    functions built and TDD for totalCost()

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
+
 function Order() {
   this.pizzas = {};
+  this.currentId = 0;
 }
 
-Order.prototype.addPizza = function(pizza) {
-  this.pizzas[pizza.size] = pizza
-  //or if we get id's involved its this.pizzas[pizza.id] = contact
-}
+
+Order.prototype.addContact = function(pizza) {
+  pizza.id = this.pizzaId();
+  this.pizzas[pizza.id] = pizza;
+};
+
+Order.prototype.pizzaId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+
+
+
+
 
 function Pizza(size, toppings) {
   this.size = size
   this.toppings = toppings
   this.items = []
-}
+};
 
-function Cost(pizza) {
+function cost(pizza) {
   let cost = 10
   if (pizza.size === "large") {
     cost += 4
@@ -25,12 +38,12 @@ function Cost(pizza) {
     cost += pizza.toppings
   }
   return cost
-}
+};
 
 
 
 // User Interface Logic
-let allPizzas = new Order()
+let allPizzas = new Order();
 
 function handleFormSubmission(event) {
   event.preventDefault();
@@ -42,10 +55,11 @@ function handleFormSubmission(event) {
   newpizza.items.push("pineapple")
   console.log(newpizza)
   console.log(newpizza.items.length)
-  allPizzas.addPizza(newpizza)
+  allPizzas.addContact(newpizza)
   console.log(allPizzas)
-}
+  console.log(cost(newpizza))
+};
 
 window.addEventListener("load", function() {
   document.querySelector("form#new-pizza").addEventListener("submit", handleFormSubmission);
-})
+});
